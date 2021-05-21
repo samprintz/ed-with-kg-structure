1013bc8aaac84cbfbacf0c3fb94af61921ce05c2
Samuel Printz <sam.printz@web.de>
2021-05-21 11:32:21 +0200

    Improve logging and path declaration for BERT and RNN triplets model
    Also train with validation and training set simultaneously.

--- bert_and_rnn_triplets_model_keras_tf2/wikidata_query/train.py
+++ bert_and_rnn_triplets_model_keras_tf2/wikidata_query/train.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import os
 import json
@@ -7,46 +8,77 @@ from wikidata_query.gcn_qa_model import GCN_QA
 from wikidata_query.read_data import get_json_data
 from wikidata_query.utils import get_words, infer_vector_from_word
 
-_path = os.path.dirname(__file__)
-_saving_dir = os.path.join(_path, '../data')
+dirs = {
+    'logging' : os.path.join(os.getcwd(), 'log'),
+    'models' : os.path.join(os.getcwd(), 'data', 'models'),
+    'datasets' : os.path.join(os.getcwd(), '..', 'dataset')
+    }
 
-if not os.path.exists(_saving_dir):
-    os.makedirs(_saving_dir)
+datasets = {
+    #'train' : os.path.join(dirs['datasets'], 'wikidata-disambig-train.json'),
+    #'train' : os.path.join(dirs['datasets'], 'wikidata-disambig-train.medium.json'),
+    'train' : os.path.join(dirs['datasets'], 'wikidata-disambig-train.sample.json'),
+    #'val' : os.path.join(dirs['datasets'], 'wikidata-disambig-dev.json')
+    'val' : os.path.join(dirs['datasets'], 'wikidata-disambig-dev.sample.json')
+    }
 
-_logger = logging.getLogger(__name__)
-_logging_level = logging.INFO
-logging.basicConfig(level=_logging_level, format="%(asctime)s: %(levelname)-1.1s %(name)s] %(message)s")
-
-_dataset_path = os.path.join(_path, '../../dataset/wikidata-disambig-train.json')
-#_dataset_path = os.path.join(_path, '../../dataset/wikidata-disambig-train.medium.json')
-#_dataset_path = os.path.join(_path, '../../dataset/wikidata-disambig-train.sample.json')
-
+_model_name = 'model-20210521-1'
+_epochs = 3
+_batch_size = 32
+_dropout = 1.0
 
 _is_relevant = [.0, 1.]
 _is_not_relevant = [1., 0.]
 
+# Logging
+log_level = logging.INFO
+log_timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_filename = f'{log_timestamp}-{_model_name}-train'
+log_path = os.path.join(dirs['logging'], f'{log_filename}.log')
+log_format = "%(asctime)s: %(levelname)-1.1s %(name)s:%(lineno)d] %(message)s"
+logging.basicConfig(level=log_level, format=log_format,
+        handlers=[logging.FileHandler(log_path), logging.StreamHandler()])
+_logger = logging.getLogger()
+
+# Create directories
+for path in dirs.values():
+    if not os.path.exists(path):
+        _logger.info(f'Create directory {path}')
+        os.makedirs(path)
+
 
 def train(data, model, saving_dir, name_prefix, epochs=20, batch_size=32):
-    dataset = {
-            'text': [item['text'] for item in data],
-            'node_vectors': [item['graph']['vectors'] for item in data],
-            'item_vector': [item['item_vector'] for item in data],
-            'question_vectors': [item['question_vectors'] for item in data],
-            'question_mask': [item['question_mask'] for item in data],
-            'y': [item['answer'] for item in data]
-    }
+    datasets = []
+    for dataset in data:
+        dataset_reshaped = {
+                'text': [item['text'] for item in dataset],
+                'node_vectors': [item['graph']['vectors'] for item in dataset],
+                'item_vector': [item['item_vector'] for item in dataset],
+                'question_vectors': [item['question_vectors'] for item in dataset],
+                'question_mask': [item['question_mask'] for item in dataset],
+                'y': [item['answer'] for item in dataset]
+        }
+        datasets.append(dataset_reshaped)
 
     saving_path = f'{saving_dir}/{name_prefix}'
-    model.train(dataset, saving_path, epochs, batch_size)
+    model.train(datasets, saving_path, epochs, batch_size)
 
 
 if __name__ == '__main__':
-    with open(_dataset_path, encoding='utf8') as f:
-        json_data = json.load(f)
-    data = get_json_data(json_data)
-    model = GCN_QA(dropout=1.0)
-    train(data, model, _saving_dir,
-            name_prefix='model-20210428-1',
-            epochs=60,
-            batch_size=32
+    # train dataset
+    with open(datasets['train'], encoding='utf8') as f:
+        json_data_train = json.load(f)
+    data_train = get_json_data(json_data_train, pbg=False)
+    # validation dataset
+    with open(datasets['val'], encoding='utf8') as f:
+        json_data_val = json.load(f)
+    data_val = get_json_data(json_data_val)
+    data = [data_train, data_val]
+
+    # train
+    model = GCN_QA(dropout=_dropout)
+    train(data, model, dirs['models'],
+            name_prefix=_model_name,
+            epochs=_epochs,
+            batch_size=_batch_size
     )
